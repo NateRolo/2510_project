fa4a3ee8ce99a3870ee20d67efbda0dd5c17093f
Arsh Mokha <amokha@my.bcit.ca>
2025-02-10 20:56:38 -0800

    feat: create doctor.h file

---
+++ doctor.h
@@ -0,0 +1,15 @@
+//
+// Created by arshd on 2/10/2025.
+//
+
+#ifndef DOCTOR_H
+#define DOCTOR_H
+
+typedef struct
+{
+    int doctorId;
+    char name[100];
+    int age;
+} Doctor;
+
+#endif //DOCTOR_H
