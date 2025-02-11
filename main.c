f0f2f4624ca1f34a8267ff27ee5f533a30bfcfda
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-02-10 19:33:46 -0800

    chore: delete redundant constants from main

--- main.c
+++ main.c
@@ -18,23 +18,6 @@
 
 #define INVALID_USER_INPUT (-1)
 
-#define MAX_PATIENT_CAPACITY 50
-
-#define MAX_ROOM_NUMBER 50
-#define MIN_ROOM_NUMBER 1
-
-#define MIN_PATIENT_NAME_LENGTH 1
-#define MAX_PATIENT_NAME_LENGTH 100
-
-#define MIN_DIAGNOSIS_LENGTH 1
-#define MAX_DIAGNOSIS_LENGTH 255
-
-#define MIN_ROOM_NUMBER 1
-#define MAX_ROOM_NUMBER 50
-
-#define MIN_AGE_YEARS 0
-#define MAX_AGE_YEARS 120
-
 // function prototypes
 void menu();
 
