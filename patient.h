c9acc3b59ba9c4f088237a15001f5dea82775f0a
Arsh Mokha <amokha@my.bcit.ca>
2025-02-12 22:30:38 -0800

    refactor: reassign them to static constexpr in c files

--- patient.h
+++ patient.h
@@ -6,59 +6,7 @@
  */
 
 #ifndef HOSPITAL_PATIENT_H
-#define HOSPTAL_PATIENT_H
-
-#define MAX_PATIENT_CAPACITY 50
-
-#define MAX_ROOM_NUMBER 50
-#define MIN_ROOM_NUMBER 1
-
-#define MIN_PATIENT_NAME_LENGTH 1
-#define MAX_PATIENT_NAME_LENGTH 100
-
-#define MIN_DIAGNOSIS_LENGTH 1
-#define MAX_DIAGNOSIS_LENGTH 255
-
-#define MIN_ROOM_NUMBER 1
-#define MAX_ROOM_NUMBER 50
-
-#define MIN_AGE_YEARS 0
-#define MAX_AGE_YEARS 120
-
-#define PATIENT_NOT_FOUND -1
-
-#define DEFAULT_ID 1
-#define INVALID_ID 0
-
-#define NULL_TERMINATOR 0
-
-#define REMOVE_PATIENT_ARRAY_MAX 49
-#define NEXT_INDEX_OFFSET 1
-
-#define ROOM_UNOCCUPIED -1
-
-#define NUMBER_ZERO 0
-#define NUMBER_NINE 9
-
-#define NO_LETTERS 0
-#define HAS_LETTERS 1
-
-#define IS_EMPTY 0
-
-/*
- * Struct: Patient
- * ----------------
- * Represents a patient in the hospital system, storing basic details
- * such as ID, name, age, diagnosis, and assigned room number.
- */
-typedef struct
-{
-    int patientId;                           
-    char name[MAX_PATIENT_NAME_LENGTH];      
-    int ageInYears;                                 
-    char diagnosis[MAX_DIAGNOSIS_LENGTH];    
-    int roomNumber;                          
-} Patient;
+#define HOSPITAL_PATIENT_H
 
 // Function prototypes for patient management
 
