eef1e4dbe302f252ddc90162bc6ce0478f34fc48
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-02-12 17:52:51 -0800

    refactor: move constants to util.h

--- utils.h
+++ utils.h
@@ -7,6 +7,15 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#define IS_NOT_VALID 0
+#define IS_VALID 1
+
+#define SUCCESSFUL_READ 1
+
+#define YES 'y'
+#define NO 'n'
+
+
 /*
  * Function: clearInputBuffer
  * --------------------------
