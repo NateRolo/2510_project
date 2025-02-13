8518ddb1e25a91b5fbbc02f12019923e7e01a682
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-02-12 17:51:57 -0800

    refactor: moved constants into header file

--- doctor.c
+++ doctor.c
@@ -12,31 +12,6 @@
 #include "utils.h"
 
 /*
- * Constants used throughout the doctor scheduling system.
- * Defines values for input validation, scheduling dimensions, and status codes.
- */
-#define INVALID_INPUT (-1)
-#define DAYS_IN_WEEK 7
-#define TIMES_OF_DAY 3
-#define DOCTOR_COUNT 3
-#define SUCCESSFUL_READ 1
-#define NAME_LENGTH 100
-#define UNASSIGNED_ID 0
-#define MIN_INDEX 0
-#define YES 'y'
-#define NO 'n'
-#define NO_DOCTOR 0
-
-#define RAYMOND_ID 10
-#define GEORGE_ID 20
-#define SOFIA_ID 30
-
-#define RAYMOND_AGE 44
-#define GEORGE_AGE 67
-#define SOFIA_AGE 33
-
-
-/*
  * Structure representing a doctor in the system.
  * Contains basic identifying information and personal details.
  */
