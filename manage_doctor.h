80063cfa4f87d39693ec36124ddc001b625d6afb
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-04 14:35:02 -0800

    refactor: created manage_doctor.h

---
+++ manage_doctor.h
@@ -0,0 +1,16 @@
+/*
+ * Author: Nathan Oloresisimo
+ * Date: 2/13/2025
+ * Purpose:
+ */
+
+#ifndef MANAGE_DOCTOR_H
+#define MANAGE_DOCTOR_H
+
+#define RAYMOND_ID 10
+#define GEORGE_ID 20
+#define SOFIA_ID 30
+#define RAYMOND_AGE 44
+#define GEORGE_AGE 67
+#define SOFIA_AGE 33
+#endif //MANAGE_DOCTOR_H
