ab64422fd5cd88789eb00f404c63d1db8c981b15
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-04 14:34:52 -0800

    refactor: created manage_doctor.c

---
+++ manage_doctor.c
@@ -0,0 +1,13 @@
+/*
+ * Author: Nathan Oloresisimo
+ * Date: 2/13/2025
+ * Purpose:
+ */
+
+#include "manage_doctor.h"
+#include <stdio.h>
+#include "doctor.c"
+
+static const Doctor doctors[] = { { RAYMOND_ID, "Raymond Redington", RAYMOND_AGE },
+                                  { GEORGE_ID, "George Washington", GEORGE_AGE },
+                                  { SOFIA_ID, "Sofia Gomez", SOFIA_AGE } };
