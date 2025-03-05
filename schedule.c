8d17c68fc8a32808ac3fad4e7672e8287e303bf2
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-04 16:26:25 -0800

    refactor: rename to schedule.c/schedule.h

--- schedule.c
+++ schedule.c
@@ -4,8 +4,8 @@
  * Purpose:
  */
 
-#include "manage_doctor.h"
 #include <stdio.h>
+#include "schedule.h"
 #include "doctor.c"
 
 static const Doctor doctors[] = { { RAYMOND_ID, "Raymond Redington", RAYMOND_AGE },
